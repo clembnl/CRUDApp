9ac09e96da92a219fba44c71e194039475cb19c6
clembnl <cl.bonal@free.fr>
2022-03-28 21:50:01 +0200

    front first commit

---
+++ crudapp-ui/src/components/Employee.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="employee">
+        <p> {{ employee.name }} </p>
+        <p> {{ employee.email }} </p>
+        <p> {{ employee.adress }} </p>
+        <p> {{ employee.arrival }} </p>
+        <Actions />
+    </div>    
+</template>
+
+<script>
+import Actions from './Actions'
+
+export default {
+    name: 'Employee',
+    components: {
+        Actions
+    },
+    props: {
+        employee: Object
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+#employee {
+    display: flex;
+    justify-content: space-around;
+}
+
+#employee :hover {
+    background-color: rgb(180, 180, 180);
+}
+</style>
---
+++ crudapp-ui/src/components/Employees.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="head">
+        <p>Name</p>
+        <p>Email</p>
+        <p>Adress</p>
+        <p>Arrival</p>
+    </div>
+    <div :key="employee.id" v-for="employee in employees">
+        <Task @update-employee="$emit('update-employee', employee)"
+        @delete-employee="$emit('delete-task', task.id)" 
+        :employee="employee" />
+    </div>    
+</template>
+
+<script>
+import Employee from './Employee'
+
+export default {
+    name: 'Employees',
+    components: {
+        Employee
+    },
+    props: {
+        employees: Array
+    },
+    emits: ['update-employee', 'delete-task']
+    
+}
+</script>
+
+<style>
+#head {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
